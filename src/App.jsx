46501484f99a76585c6bebda61263b31919edc99
Abhishek Jha <jhaa213152@gmail.com>
2025-03-03 23:08:20 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home";
+import Ats from "./Ats";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ats" element={<Ats />} />
       </Routes>
     </div>
   );
